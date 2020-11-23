c26f428e079265af6d7552da35908ae75cc5189f
joshuaapr <64120565+joshuaapr@users.noreply.github.com>
2020-11-23 14:36:53 +0700

    Add files via upload
    Updated 1

--- app.js
+++ app.js
@@ -1,3 +1,99 @@
-const calculator = console.log('test');
+// const calculator = console.log('test');
 
-calculator();
+// calculator;
+
+const valueEl = document.querySelector('.value');
+
+const acEl = document.querySelector('.ac');
+const pmEl = document.querySelector('.pm');
+const percentEl = document.querySelector('.percent');
+
+const additionEl = document.querySelector('.addition');
+const substractionEl = document.querySelector('.substraction');
+const multiplicationEl = document.querySelector('.multiplication');
+const divisionEl = document.querySelector('.division');
+const equalEl = document.querySelector('.equal');
+
+const number0El = document.querySelector('.number-0');
+const number1El = document.querySelector('.number-1');
+const number2El = document.querySelector('.number-2');
+const number3El = document.querySelector('.number-3');
+const number4El = document.querySelector('.number-4');
+const number5El = document.querySelector('.number-5');
+const number6El = document.querySelector('.number-6');
+const number7El = document.querySelector('.number-7');
+const number8El = document.querySelector('.number-8');
+const number9El = document.querySelector('.number-9');
+const numberElArray = [
+  number0El,
+  number1El,
+  number2El,
+  number3El,
+  number4El,
+  number5El,
+  number6El,
+  number7El,
+  number8El,
+  number9El,
+];
+const decimalEl = document.querySelector('.decimal');
+
+// Memory
+let valueStrInMemory = null;
+let operatorInMemory = null;
+
+// Function
+const getValueAsStr = () => valueEl.textContent.split(',').join('');
+
+const getValueAsNum = () => {
+  return parseFloat(getValueAsStr());
+};
+
+const setStrAsValue = (valueStr) => {
+  if (valueStr[valueStr.length - 1] === '.') {
+    valueEl.textContent += '.';
+    return;
+  }
+  const [wholeNumStr, decimalStr] = valueStr.split('.');
+  if (decimalStr) {
+    valueEl.textContent = parseFloat(wholeNumStr).toLocaleString() + '.' + decimalStr;
+  } else {
+    valueEl.textContent = parseFloat(wholeNumStr).toLocaleString();
+  }
+};
+
+const handleNumberClick = (numStr) => {
+  const currentValueStr = getValueAsStr();
+  if (currentValueStr === '0') {
+    setStrAsValue(numStr);
+  } else {
+    setStrAsValue(currentValueStr + numStr);
+  }
+};
+
+// const handleOperationClick = (operation) => {
+//   const currentValueStr = getValueAsStr();
+
+//   if (!valueStrInMemory) {
+//     valueStrInMemory = currentValueStr;
+//     // operatorInMemory =operation;
+//     setStrAsValue('0');
+//     return;
+//   }
+//   setStrAsValue('0');
+// }
+
+// Add Event Listener
+acEl.addEventListener('click', () => {
+  setStrAsValue('0');
+  valueStrInMemory = null;
+  operatorInMemory = null;
+  console.log('AC');
+});
+
+for (let i=0; i < numberElArray.length; i++) {
+  const numberEl = numberElArray[i];
+  numberEl.addEventListener('click', () => {
+    handleNumberClick(i.toString());
+  });
+}
